a70e65ba19d2d2798abfa78c340d9d8c76f32ade
Lunatico0 <patipita.98@gmail.com>
2025-10-22 03:39:23 -0300

    Add CardWithIconProps type definition and remove HighlightItem interface

--- src/types/highlight.d.ts
+++
@@ -1,9 +0,0 @@
-import type { SvgIconTypeMap } from '@mui/material';
-import type { OverridableComponent } from '@mui/material/OverridableComponent';
-
-export interface HighlightItem {
-  Icon: OverridableComponent<SvgIconTypeMap<object, "svg">>;
-  title: string;
-  paragraph: string;
-  index?: number;
-}
