0ddaad0ab828ccab44f211e9ef9c7b14742ae77f
Lunatico0 <patipita.98@gmail.com>
2025-10-28 23:14:49 -0300

    Remove commented-out 'allowedDevOrigins' from Next.js configuration for cleaner code.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
-    allowedDevOrigins: ['http://192.168.1.2:3000'],
+    // allowedDevOrigins: ['http://192.168.1.2:3000'],
   },
 };
 
