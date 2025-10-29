89a02aaa4f69c925f2bcc67ff640ca4a1c59b979
Lunatico0 <patipita.98@gmail.com>
2025-10-28 23:12:35 -0300

    Update configuration, package details, and add new CV files
    - Added 'allowedDevOrigins' to the Next.js configuration for development purposes.
    - Updated project name to "CodeByPittana" and version to 0.2.0 in package.json and package-lock.json.
    - Introduced 'react-hot-toast' dependency for improved user notifications.
    - Added TypeScript path alias for utilities in tsconfig.json.
    - Created new CV files in HTML format and a CV image for enhanced presentation of professional credentials.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
+    allowedDevOrigins: ['http://192.168.1.2:3000'],
   },
 };
 
