181eaa93c4b70363edc6326c13016af5e03364e1
Lunatico0 <patipita.98@gmail.com>
2025-10-28 23:34:55 -0300

    Add Suspense fallback to ContactSection for improved loading experience

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,13 @@
 import ContactSection from "@sections/Contact/ContactSection";
+import { Suspense } from 'react';
 
 export default function ContactPage() {
   return (
+    // Es buena práctica darle un fallback al Suspense
     <main className="flex min-h-screen flex-col items-center p-24">
-      <ContactSection />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <ContactSection />
+      </Suspense>
     </main>
   );
 }
